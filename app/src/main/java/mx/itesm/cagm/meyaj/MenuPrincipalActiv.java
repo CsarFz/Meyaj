4253dbff8d1a6168ee723f38fd0576f99fe26266
César Medrano <cesargalvanmedrano@macbook-pro-de-cesar.local>
2018-03-10 10:55:53 -0600

    Iconos del menú, fragmentos de las pantallas, diseño de la pantalla PERFIL, colores de la aplicación.

--- app/src/main/java/mx/itesm/cagm/meyaj/MenuPrincipalActiv.java
+++ app/src/main/java/mx/itesm/cagm/meyaj/MenuPrincipalActiv.java
@@ -3,13 +3,16 @@ package mx.itesm.cagm.meyaj;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MenuPrincipalActiv extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private boolean estaEnBuscar; // Para saber que fragmento mostrar
+    private boolean estaEnAgenda;
+    private boolean estaEnPerfil;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +20,32 @@ public class MenuPrincipalActiv extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navBuscar:
+                    if (!estaEnBuscar) {   // Si no está mostrando el fragmento, lo muestra
+                        BuscarFrag fragCaptura = new BuscarFrag();
+                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.frContenedor, fragCaptura);
+                        ft.commit();
+                        estaEnBuscar = true;
+                    }
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navAgenda:
+                    if (estaEnBuscar) {   // Si no está mostrando el fragmento, lo muestra
+                        AgendaFrag fragCaptura = new AgendaFrag();
+                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.frContenedor, fragCaptura);
+                        ft.commit();
+                        estaEnBuscar = false;
+                    }
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navPerfil:
+                    if (estaEnBuscar) {   // Si no está mostrando el fragmento, lo muestra
+                        PerfilFrag fragCaptura = new PerfilFrag();
+                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                        ft.replace(R.id.frContenedor, fragCaptura);
+                        ft.commit();
+                        estaEnBuscar = false;
+                    }
                     return true;
             }
             return false;
@@ -36,9 +57,15 @@ public class MenuPrincipalActiv extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Fragmento inicial (Buscar servicios)
+        BuscarFrag fragBuscar = new BuscarFrag();
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frContenedor, fragBuscar);
+        ft.commit();
+        estaEnBuscar = true;
     }
 
 }
