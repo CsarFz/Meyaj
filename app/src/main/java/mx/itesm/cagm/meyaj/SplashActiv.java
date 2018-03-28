26fc62d3c049ea63ec4a895c42e3b38582a3f501
César Galván <a01374257@gmail.com>
2018-03-28 12:55:36 -0600

    Modificación de diseño de la pantalla buscar, implementación de pantalla de carga y nuevas imágenes para los iconos.

---
+++ app/src/main/java/mx/itesm/cagm/meyaj/SplashActiv.java
@@ -0,0 +1,39 @@
+package mx.itesm.cagm.meyaj;
+
+import android.app.Fragment;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActiv extends AppCompatActivity
+{
+    private ImageView iv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        iv = findViewById(R.id.ivLogo);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.transition);
+        iv.startAnimation(animation);
+        final Intent intMenuPrincipal = new Intent(this, MenuPrincipalActiv.class);
+        Thread timer = new Thread(){
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intMenuPrincipal);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
