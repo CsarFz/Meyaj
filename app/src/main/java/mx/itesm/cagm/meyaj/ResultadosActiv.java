7316af03f804f07e38ad5f55de1fe9dcf0ed40ac
Rodrigo Nvz <a01377566@itesm.mx>
2018-04-03 17:41:57 -0500

    Detalle profesionista

--- app/src/main/java/mx/itesm/cagm/meyaj/ResultadosActiv.java
+++ app/src/main/java/mx/itesm/cagm/meyaj/ResultadosActiv.java
@@ -1,7 +1,10 @@
 package mx.itesm.cagm.meyaj;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
@@ -10,8 +13,8 @@ public class ResultadosActiv extends AppCompatActivity {
 
     ListView lista;
 
-    String[][] datosProfesionista = {
-            {"Nombre","Profesion", "11.6", "8", "45"},
+    String[][] datosProfesionistas = {
+            {"Nombre","Profesion", "11.6", "8", "45","Direccion"},
             {"Nombre","Profesion", "12.5", "8", "35"},
             {"Nombre","Profesion", "20.9", "6", "19"},
             {"Nombre","Profesion", "30.8", "9", "101"},
@@ -27,7 +30,19 @@ public class ResultadosActiv extends AppCompatActivity {
         setContentView(R.layout.activity_resultados);
 
         lista = (ListView) findViewById(R.id.lvProfesionistas);
+        lista.setAdapter(new Adaptador(this,datosProfesionistas,imgProfesionistas));
 
-        lista.setAdapter(new Adaptador(this,datosProfesionista,imgProfesionistas));
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent visorDetalles = new Intent(view.getContext(), DetalleProfesionistaActiv.class);
+                visorDetalles.putExtra("NOM", datosProfesionistas[i][0]);
+                visorDetalles.putExtra("TIT", datosProfesionistas[i][0]);
+                visorDetalles.putExtra("TIT", datosProfesionistas[i][0]);
+                visorDetalles.putExtra("TIT", datosProfesionistas[i][0]);
+                startActivity(visorDetalles);
+            }
+        });
+
     }
 }
