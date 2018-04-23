7935967865d294e688a267f5b0e4882dfe6306ad
César Galván <A01374257@gmail.com>
2018-04-23 10:37:03 -0500

    Modificación de la pantalla agenda

--- app/src/main/java/mx/itesm/cagm/meyaj/SplashActiv.java
+++ app/src/main/java/mx/itesm/cagm/meyaj/SplashActiv.java
@@ -4,9 +4,11 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+
 
 public class SplashActiv extends AppCompatActivity
 {
@@ -20,12 +22,13 @@ public class SplashActiv extends AppCompatActivity
         iv = findViewById(R.id.ivLogo);
 
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.transition);
+
         iv.startAnimation(animation);
         final Intent intMenuPrincipal = new Intent(this, MenuPrincipalActiv.class);
         Thread timer = new Thread(){
             public void run() {
                 try {
-                    sleep(4000);
+                    sleep(2000);
                 } catch(InterruptedException e) {
                     e.printStackTrace();
                 } finally {
