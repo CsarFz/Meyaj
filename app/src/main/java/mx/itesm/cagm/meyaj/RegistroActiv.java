1ae0200918a7c7e3217c2980d8c1ccf6e0002972
Isain965 <isain_5@hotmail.com>
2018-04-26 11:20:34 -0500

    Registro de Usuarios (unicamente usuario y contraseña) en la plataforma Google Firebase

--- app/src/main/java/mx/itesm/cagm/meyaj/RegistroActiv.java
+++ app/src/main/java/mx/itesm/cagm/meyaj/RegistroActiv.java
@@ -1,17 +1,26 @@
 package mx.itesm.cagm.meyaj;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.StringRequestListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistroActiv extends AppCompatActivity {
 
@@ -25,9 +34,29 @@ public class RegistroActiv extends AppCompatActivity {
     private Button btnEnviar;
     private Button btnCancelar;
 
+
+
+
+    private ProgressDialog progressDialog;
+
+
+    private FirebaseAuth firebaseAuth;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        progressDialog = new ProgressDialog(this);
+
+
+
+
         setContentView(R.layout.activity_registro);
         etUsuario = findViewById(R.id.etUsuario);
         etNombre = findViewById(R.id.etNombre);
@@ -41,7 +70,7 @@ public class RegistroActiv extends AppCompatActivity {
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                enviarADrive();
+                registrarUsuario();
             }
         });
 
@@ -58,6 +87,67 @@ public class RegistroActiv extends AppCompatActivity {
     public void onStart() {
         super.onStart();
 
+    }
+
+    private void registrarUsuario(){
+        String usuario = etUsuario.getText().toString().trim();
+        String nombre = etNombre.getText().toString().trim();
+        String apellido = etApellido.getText().toString().trim();
+        String edad = etEdad.getText().toString().trim();
+        String correo = etCorreo.getText().toString().trim();
+        String ocupacion = etOcupacion.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+
+        //Comprobar que los campos esten llenos
+        if(TextUtils.isEmpty(usuario)){
+            Toast.makeText(getApplicationContext(), "Este campo debe estar lleno", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(nombre)){
+            Toast.makeText(getApplicationContext(), "Este campo debe estar lleno", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(apellido)){
+            Toast.makeText(getApplicationContext(), "Este campo debe estar lleno", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(edad)){
+            Toast.makeText(getApplicationContext(), "Este campo debe estar lleno", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(correo)){
+            Toast.makeText(getApplicationContext(), "Este campo debe estar lleno", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(ocupacion)){
+            Toast.makeText(getApplicationContext(), "Este campo debe estar lleno", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            Toast.makeText(getApplicationContext(), "Este campo debe estar lleno", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+
+        progressDialog.setMessage("Registrando usuario....");
+        progressDialog.show();
+
+
+
+        firebaseAuth.createUserWithEmailAndPassword(correo,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //El usuario esta registrado exitosamente, vamos a abrir la actividad para llena rel perfil
+                            Toast.makeText(RegistroActiv.this,"Usuario registrado", Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(RegistroActiv.this,"No se pudo registrar, intenta de nuevo", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void enviarADrive() {
