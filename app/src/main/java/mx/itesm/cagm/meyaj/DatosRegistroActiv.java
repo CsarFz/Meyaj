2373cc5e2124e761a1f24f704014186fbe8014b9
A01375667 <A01375667@itesm.mx>
2018-05-06 21:26:43 -0500

    Se sube imagen de perfil a firebase

--- app/src/main/java/mx/itesm/cagm/meyaj/DatosRegistroActiv.java
+++ app/src/main/java/mx/itesm/cagm/meyaj/DatosRegistroActiv.java
@@ -1,20 +1,38 @@
 package mx.itesm.cagm.meyaj;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class DatosRegistroActiv extends AppCompatActivity
 {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageButton btnSubirImagen;
+    private Uri imageUri;
+    private StorageReference mStorageRef;
+
     private Button btnGuardarPerfil;
     private EditText etNombre;
     private EditText etApellidos;
@@ -32,7 +50,7 @@ public class DatosRegistroActiv extends AppCompatActivity
 
         firebaseAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference();
-
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         etNombre = findViewById(R.id.etNombreCP);
         etApellidos = findViewById(R.id.etApellidosCP);
@@ -49,19 +67,26 @@ public class DatosRegistroActiv extends AppCompatActivity
             }
         });
 
-        /*
-        btnCerrarSesion = findViewById(R.id.btnCerrarSesionP);
-        btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
+
+        btnSubirImagen = findViewById(R.id.btnImagen);
+        btnSubirImagen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                firebaseAuth.signOut();
-                finish();
-                startActivity(new Intent(getApplicationContext(), MenuPrincipalActiv.class));
+                openFileChooser();
             }
         });
-        */
 
     }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+
+
 
     private void guardarInformacion(){
         String nombre = etNombre.getText().toString().trim();
@@ -69,15 +94,44 @@ public class DatosRegistroActiv extends AppCompatActivity
         String numTel = etNumeroTel.getText().toString().trim();
         String direccion = etDireccion.getText().toString().trim();
 
-        UserInformation userInformation = new UserInformation(nombre,apellido,numTel,direccion);
+        if(nombre == "" || apellido == "" || numTel == "" || direccion == ""){
+            Toast.makeText(this, "Llena todos los campos.", Toast.LENGTH_SHORT).show();
+        } else if(imageUri == null){
+            Toast.makeText(this, "Sube una foto.", Toast.LENGTH_SHORT).show();
+        } else {
+            UserInformation userInformation = new UserInformation(nombre, apellido, numTel, direccion);
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            databaseReference.child(user.getUid()).setValue(userInformation);
 
-        FirebaseUser user = firebaseAuth.getCurrentUser();
+            StorageReference fileReference = mStorageRef.child(user.getUid() + "/" + "profilepic.jpg");
+            fileReference.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //Toast.makeText(DatosRegistroActiv.this, "Se subió la imagen", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(DatosRegistroActiv.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
 
-        databaseReference.child(user.getUid()).setValue(userInformation);
-
-        Toast.makeText(this,"Guardando informacion....", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(DatosRegistroActiv.this, MenuPrincipalActiv.class);
-        startActivity(intent);
-
+            Toast.makeText(this, "Creando perfil....", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(DatosRegistroActiv.this, MenuPrincipalActiv.class);
+            startActivity(intent);
+        }
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+            btnSubirImagen.setImageURI(imageUri);
+        }
+    }
+
 }
--- app/src/main/java/mx/itesm/cagm/meyaj/DatosRegistroProfesionista.java
+++ app/src/main/java/mx/itesm/cagm/meyaj/DatosRegistroProfesionista.java
@@ -1,20 +1,33 @@
 package mx.itesm.cagm.meyaj;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class DatosRegistroProfesionista extends AppCompatActivity
 {
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageButton btnSubirImagen;
+    private Uri imageUri;
+    private StorageReference mStorageRef;
+
     private Button btnGuardarPerfil;
     private EditText etNombre;
     private EditText etApellidos;
@@ -33,6 +46,7 @@ public class DatosRegistroProfesionista extends AppCompatActivity
 
         firebaseAuth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
 
         etNombre = findViewById(R.id.etNombreCP);
@@ -50,6 +64,30 @@ public class DatosRegistroProfesionista extends AppCompatActivity
 
             }
         });
+
+        btnSubirImagen = findViewById(R.id.btnImagen);
+        btnSubirImagen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+    }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null){
+            imageUri = data.getData();
+            btnSubirImagen.setImageURI(imageUri);
+        }
     }
 
     private void guardarInformacion(){
@@ -60,13 +98,34 @@ public class DatosRegistroProfesionista extends AppCompatActivity
         String profesion = etProfesion.getText().toString().trim();
 
 
-        Profesionista profesionista = new Profesionista(nombre,apellido,numTel,direccion,profesion);
+        if(nombre == "" || apellido == "" || numTel == "" || direccion == "" || profesion == ""){
+            Toast.makeText(this, "Llena todos los campos.", Toast.LENGTH_SHORT).show();
+        } else if(imageUri == null){
+            Toast.makeText(this, "Sube una foto.", Toast.LENGTH_SHORT).show();
+        } else {
 
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        databaseReference.child(user.getUid()).setValue(profesionista);
+            Profesionista profesionista = new Profesionista(nombre,apellido,numTel,direccion,profesion);
+            FirebaseUser user = firebaseAuth.getCurrentUser();
+            databaseReference.child(user.getUid()).setValue(profesionista);
 
-        Toast.makeText(this,"Guardando informacion....", Toast.LENGTH_LONG).show();
-        Intent intent = new Intent(DatosRegistroProfesionista.this, MenuPrincipalActiv.class);
-        startActivity(intent);
+            StorageReference fileReference = mStorageRef.child(user.getUid() + "/" + "profilepic.jpg");
+            fileReference.putFile(imageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            //Toast.makeText(DatosRegistroActiv.this, "Se subió la imagen", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(DatosRegistroProfesionista.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+            Toast.makeText(this,"Creando perfil....", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(DatosRegistroProfesionista.this, MenuPrincipalActiv.class);
+            startActivity(intent);
+        }
     }
 }
